ba204168acf0f1eb838be918088762a56b4b00d2
Manuel Nucci <32201854+Manuxi777@users.noreply.github.com>
2018-11-23 07:57:10 -0300

    Cambios a las funcionalidades

--- Funcionalidades/consulta_1.sql
+++ Funcionalidades/consulta_1.sql
@@ -1,30 +1,11 @@
--- 1 - Consulta (división con EXCEPT)
-SELECT empleado.nombre, 
-       empleado.apellido, 
-       empleado.id_empleado 
-FROM   empleado 
-/* No haría falta este INNER JOIN ya que el INNER JOIN de la segunda subquery me filtra a los empleados
-   no profesionales */ 
-/* INNER JOIN empleado_no_profesional ON empleado.id_empleado = empleado_no_profesional.id_empleado */
-WHERE  NOT EXISTS ((SELECT area.num_area 
-                    FROM   area 
-                    WHERE  area.id_nivel_seg = empleado.id_nivel_seg) 
-                   EXCEPT 
-                   (SELECT acceso.num_area 
-                    FROM   acceso 
-                           /* Tengo la garantía que en la tabla acceso sólo aparecen id's de empleados no profesionales */
-                           INNER JOIN empleado 
-                                   ON acceso.id_empleado = empleado.id_empleado));
-
 -- 1 - Consulta (división con la forma enseñada por Leticia)
 SELECT empleado.nombre, 
        empleado.apellido, 
        empleado.id_empleado 
 FROM   empleado 
 WHERE  NOT EXISTS (SELECT * 
-                   FROM   area 
-                   WHERE  area.id_nivel_seg = empleado.id_nivel_seg 
-                          AND NOT EXISTS (SELECT * 
-                                          FROM   acceso 
-                                                 INNER JOIN empleado 
-                                                         ON acceso.id_empleado = empleado.id_empleado));
+                   FROM area 
+                   WHERE NOT EXISTS (SELECT * 
+                                     FROM acceso 
+                                     WHERE acceso.id_empleado = empleado.id_empleado AND
+                                           acceso.num_area = area.num_area));
--- Funcionalidades/consulta_3.sql
+++ Funcionalidades/consulta_3.sql
@@ -5,8 +5,10 @@ SELECT empleado.id_empleado,
 FROM empleado
 INNER JOIN registro ON empleado.id_empleado = registro.id_empleado
 INNER JOIN area ON registro.num_area = area.num_area
+INNER JOIN nivel_seguridad NE ON empleado.id_nivel_seg = NE.id_nivel_seg
+INNER JOIN nivel_seguridad NA ON area.num_area = NA.id_nivel_seg
 WHERE DATEDIFF(DAY, CONVERT(date, registro.fecha_hora, 101), GETDATE()) <= 30
-      AND empleado.id_nivel_seg = area.id_nivel_seg 
+      AND (NE.nombre = 'Alto' OR (NE.nombre = 'Medio' AND NA.nombre = 'Medio')) --Error contraseña
 	  AND registro.autorizado = 'No'
 GROUP BY empleado.id_empleado, empleado.nombre, empleado.apellido, empleado.documento
 HAVING COUNT(*) > 5
@@ -18,7 +20,7 @@ SELECT DISTINCT empleado.id_empleado,
 				empleado.apellido, 
 				empleado.documento
 FROM empleado
-INNER JOIN registro ON empleado.id_empleado = registro.id_empleado
+INNER JOIN registro ON empleado.id_empleado = registro.id_empleado AND empleado.id_nivel_seg <> '1'
 INNER JOIN area ON registro.num_area = area.num_area
 INNER JOIN nivel_seguridad NE ON empleado.id_nivel_seg = NE.id_nivel_seg
 INNER JOIN nivel_seguridad NA ON area.num_area = NA.id_nivel_seg
