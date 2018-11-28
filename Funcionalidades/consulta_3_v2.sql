0f9723ce5ece2d0c0405719fe574c8866537f9da
Manuel Nucci <32201854+Manuxi777@users.noreply.github.com>
2018-11-28 07:49:39 -0300

    Upgrade functionalities

--- Funcionalidades/consulta_3_v2.sql
+++ Funcionalidades/consulta_3_v2.sql
@@ -4,11 +4,8 @@ SELECT empleado.id_empleado, empleado.nombre, empleado.apellido, empleado.docume
 FROM empleado
 INNER JOIN registro ON empleado.id_empleado = registro.id_empleado
 INNER JOIN area ON registro.num_area = area.num_area
-INNER JOIN nivel_seguridad NSE ON empleado.id_nivel_seg = NSE.id_nivel_seg
-INNER JOIN nivel_seguridad NSA ON area.num_area = NSA.id_nivel_seg
 WHERE DATEDIFF(DAY, CONVERT(date, registro.fecha_hora, 101), CONVERT(date, GETDATE(), 101)) <= 30
-	AND registro.autorizado = 'No'
-      AND (NSE.nombre = 'Alto' OR (NSE.nombre = 'Medio' AND NSA.nombre = 'Medio')) --Error contraseña
+	  AND registro.autorizado = 'No'
 GROUP BY empleado.id_empleado, empleado.nombre, empleado.apellido, empleado.documento
 HAVING COUNT(*) > 5
 
@@ -16,11 +13,12 @@ UNION
 
 SELECT empleado.id_empleado, empleado.nombre, empleado.apellido, empleado.documento
 FROM empleado
-INNER JOIN registro ON empleado.id_empleado = registro.id_empleado AND empleado.id_nivel_seg <> '1'
+INNER JOIN registro ON empleado.id_empleado = registro.id_empleado
 INNER JOIN area ON registro.num_area = area.num_area
 INNER JOIN nivel_seguridad NSE ON empleado.id_nivel_seg = NSE.id_nivel_seg
-INNER JOIN nivel_seguridad NSA ON area.num_area = NSA.id_nivel_seg
+INNER JOIN nivel_seguridad NSA ON area.id_nivel_seg = NSA.id_nivel_seg
 WHERE DATEDIFF(DAY, CONVERT(date, registro.fecha_hora, 101), CONVERT(date, GETDATE(), 101)) <= 30
+      -- AND NSE.nombre <> 'Alto'
       AND registro.autorizado = 'No'
 	  AND ((NSE.nombre = 'Bajo' AND (NSA.nombre = 'Medio' OR NSA.nombre = 'Alto'))
             OR (NSE.nombre = 'Medio' AND NSA.nombre = 'Alto'));
